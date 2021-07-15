c993973dfe1ec663833b597e1245e1af42a19c9e
Ilya Kadava <kadavaisback@gmail.com>
2021-07-16 00:58:17 +0300

    fetch данных о ракетах, вывод их названий в хедер, по клику на них меняется название и видео

--- src/App.js
+++ src/App.js
@@ -4,19 +4,54 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
+import FetchData from '../src/service/FetchData';
 // import { render } from '@testing-library/react';
 // import Calendar from './components/Calendar/Calendar';
 // import Details from './components/Details/Details';
 
 class App extends React.Component{
 
+  //на основе файла FetchData.js
+  fetchData = new FetchData();
+
+  state = {
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: "",
+  };
+
+  componentDidMount(){
+    this.updateRocket();
+  }
+
+  //получаем данные по ссылке и если item.name = state.rocket,
+  //то инфу о ракете запихиваем в rocketFeatures
+  updateRocket(){
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({rockets: data.map(item => item.name)});
+        return data
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(features => this.setState({rocketFeatures: features})
+    );
+  }
+
+  changeRocket = (rocket) => {
+    this.setState(
+      {
+        rocket
+      }, this.updateRocket
+    );
+  }
 
 
   render(){
+    // console.log(this.state.rockets)
     return (
       <>
-        <Header />
-        <Main/>
+        <Header rocketNames = {this.state.rockets} changeRocket={this.changeRocket}/>
+        <Main rocket={this.state.rocket}/>
         <Features />
         {/* <Calendar /> */}
         {/* <Details/> */}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css';
 import logo from '../../logo.svg';
 
-const Header = () => (
+const Header = props => (
     <header className="header">
         <img
             src={logo}
@@ -11,18 +11,21 @@ const Header = () => (
         />
         <nav className="main-nav nav">
             <ul className="list">
-                <li className="item">
-                    <a href="/" className="item-link">Falcon 1</a>
-                </li>
-                <li className="item">
-                    <a href="/" className="item-link">Falcon 9</a>
-                </li>
-                <li className="item">
-                    <a href="/" className="item-link">Falcon Heavy</a>
-                </li>
-                <li className="item">
+                {props.rocketNames.map((item,index) => (
+                    <li key={index} className="item">
+                        <a href="/" 
+                        onClick={e => {e.preventDefault(); props.changeRocket(item)}}
+                        className="item-link">
+                            {item}
+                        </a>
+                    </li>
+                ))}
+
+                {/* 2:20 */}
+
+                {/* <li className="item">
                     <a href="/" className="item-link">Updates</a>
-                </li>
+                </li> */}
             </ul>
         </nav>
         <nav className="secondary-nav">
