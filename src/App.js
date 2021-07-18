cf63717088bba286fd6d82f24c629d035a9f8ef1
Ilya Kadava <kadavaisback@gmail.com>
2021-07-18 18:02:06 +0300

    создал компоненту Home + css, props and info from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './style.css';
 import React from 'react';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 import Main from './components/Main/Main';
 import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
@@ -56,8 +57,9 @@ class App extends React.Component{
     return (
       <>
         <Header rocketNames = {this.state.rockets} changeRocket={this.changeRocket}/>
-        <Main rocket={this.state.rocket}/>
-        {this.state.rocketFeatures && <Features {...this.state.rocketFeatures}/>}
+        {this.state.company && <Home company={this.state.company}/> }
+        {/* <Main rocket={this.state.rocket}/> */}
+        {/* {this.state.rocketFeatures && <Features {...this.state.rocketFeatures}/>} */}
         {/* <Calendar /> */}
         {/* <Details/> */}
         {this.state.company && <Footer {...this.state.company} />}
