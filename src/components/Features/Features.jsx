c318528e78489dc36de8ff6ed53f364baee875a9
Ilya Kadava <kadavaisback@gmail.com>
2021-07-20 00:03:00 +0300

    в календаре из api достаю и вывожу данные в карточки, по кнопке в карточке открывается инфа о событии детальнее. фикс багов

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -20,52 +20,52 @@ const Features = ({
     }
 
     return (
-        <section className="features">
-            <h2 className="features-title">
-                {name} <br />Overview
-            </h2>
-            <div className="overview">
+            <section className="features">
+                <h2 className="features-title">
+                    {name} <br />Overview
+                </h2>
+                <div className="overview">
 
-                <table className="table">
-                    <caption className="table-title">
-                        Size
-                    </caption>
-                    <thead>
-                        <tr>
-                            <td className="table-column">HEIGHT</td>
-                            <td className="table-column">{height.meters} m / {height.feet} ft</td>
-                        </tr>
-                        <tr>
-                            <td className="table-column">DIAMETER</td>
-                            <td className="table-column">{diameter.meters} m / {diameter.feet} ft</td>
-                        </tr>
-                        <tr>
-                            <td className="table-column">MASS</td>
-                            <td className="table-column">{mass.kg} kg / {mass.lb} lb</td>
-                        </tr>
-                        {payload_weights.map(item => 
-                        <tr key={item.id}>
-                            <td className="table-column">PAYLOAD TO {item.id.toUpperCase()}</td>
-                            <td className="table-column">{item.kg} kg / {item.lb} lb</td>
-                        </tr>)}
-                    </thead>
-                </table>
-                <RellaxWrapper speed={14}>
-                    <img
-                        src={`img/${rocketImg[name]}.png`}
-                        alt="rocket"
-                        className="rocket"
-                        data-rellax-speed="14"
-                    />
-                </RellaxWrapper>
-                <article>
-                    <h3 className="features-subtitle">DESCRIPTION</h3>
-                    <p className="features-text">
-                        {description}
-                    </p>
-                </article>
-            </div>
-        </section>
+                    <table className="table">
+                        <caption className="table-title">
+                            Size
+                        </caption>
+                        <thead>
+                            <tr>
+                                <td className="table-column">HEIGHT</td>
+                                <td className="table-column">{height.meters} m / {height.feet} ft</td>
+                            </tr>
+                            <tr>
+                                <td className="table-column">DIAMETER</td>
+                                <td className="table-column">{diameter.meters} m / {diameter.feet} ft</td>
+                            </tr>
+                            <tr>
+                                <td className="table-column">MASS</td>
+                                <td className="table-column">{mass.kg} kg / {mass.lb} lb</td>
+                            </tr>
+                            {payload_weights.map(item => 
+                            <tr key={item.id}>
+                                <td className="table-column">PAYLOAD TO {item.id.toUpperCase()}</td>
+                                <td className="table-column">{item.kg} kg / {item.lb} lb</td>
+                            </tr>)}
+                        </thead>
+                    </table>
+                    <RellaxWrapper speed={14}>
+                        <img
+                            src={`img/${rocketImg[name]}.png`}
+                            alt="rocket"
+                            className="rocket"
+                            data-rellax-speed="14"
+                        />
+                    </RellaxWrapper>
+                    <article>
+                        <h3 className="features-subtitle">DESCRIPTION</h3>
+                        <p className="features-text">
+                            {description}
+                        </p>
+                    </article>
+                </div>
+            </section>
     );
 }
 export default Features;
